8cb0f0f1068ccccb46747b509778658a99e4f94b
bcd <bcvanduuren@gmail.com>
2013-11-07 20:06:18 +0100

    time.rb created calculate_enddate

--- lib/brush/time.rb
+++ lib/brush/time.rb
@@ -6,10 +6,18 @@ class Brush
 
             case unit
                 when 'hours'
-                    return now - (amount * (60 ** 2) * 1000)
+                    return now - (amount * (60 * 60) * 1000)
                 when 'days'
-                    return now - (amount * 24 * (60 ** 2) * 1000)
+                    return now - (amount * 24 * (60 * 60) * 1000)
             end
+        end
+
+        def calculate_enddate(days_to_keep)
+            seconds_per_day = 24 * 60 * 60
+            enddate =  Time.now()
+            enddate -= days_to_keep.to_i * seconds_per_day
+
+            return enddate.strftime("%Y.%m.%d")
         end
     end
 end
