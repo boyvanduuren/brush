b1af81b2d44c5b2d30c0eace5bc1eb1bcf6c7b1d
bcd <bcvanduuren@gmail.com>
2013-11-07 21:16:56 +0100

    Cleaned up lib/brush.rb

--- lib/brush.rb
+++ lib/brush.rb
@@ -9,47 +9,18 @@ class Brush < Thor
     include Brush::Timecalc
     include Brush::Elasticsearch
 
-    desc "version", "show the version of brush"
-    def version
-        puts "brush v0.01"
-    end
-
     desc "hours N <host>:<port>", "clean the elasticsearch backend, leaving N hours"
-    long_desc <<-LONGDESC
-        `hours N` will clean the elasticsearch backend, leaving N hours of logging intact
-
-        sample usage:
-        \x5brush hours 16 eshost:9200
-    LONGDESC
     def hours(keep_hours, uri)
         purge_hours(keep_hours, uri)
     end
 
     desc "days N <host>:<port>", "clean the elasticsearch backend, leaving N days"
-    long_desc <<-LONGDESC
-        `days N` will clean the elasticsearch backend, leaving N days of logging intact
-
-        sample usage:
-        \x5brush days 7 eshost:9200
-    LONGDESC
     def days(keep_days, uri)
         purge_days(keep_days, uri)
     end
 
     desc "space N <host>:<port>", "clean the elasticsearch backend, leaving N of space"
-    long_desc <<-LONGDESC
-        `space N` will clean the elasticsearch backend, leaving N of space left.
-
-        N may be defined as:
-
-            K for kilobytes
-            \x5M for megabytes
-            \x5G for gigabytes
-
-        sample usage:
-        \x5brush space 10G eshost:9200
-    LONGDESC
-    def space(keep_space, es_cluster)
-        puts "Cleaning the backend on #{es_cluster}, leaving #{keep_space} of space."
+    def space(keep_space, uri)
+        puts "Not implemented yet"
     end
 end
